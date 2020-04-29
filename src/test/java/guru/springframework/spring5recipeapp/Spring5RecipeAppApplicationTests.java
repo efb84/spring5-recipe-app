9044dd1aa6b5b6d5a1f8cc5c18d5b7039143f951
baykoc@gmail.com <baykoc@gmail.com>
2020-04-29 18:50:03 +0300

    edited2

--- src/test/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Spring5RecipeAppApplicationTests.class)
+@SpringBootTest
 public class Spring5RecipeAppApplicationTests {
 
 	@Test
